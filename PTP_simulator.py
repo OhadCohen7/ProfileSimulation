bcdd70a4d7172d6f6559e32d19e1b359d7821249
OhadCohen7 <130965705+OhadCohen7@users.noreply.github.com>
2025-06-30 15:32:52 +0300

    Add files via upload

--- PTP_simulator.py
+++ PTP_simulator.py
@@ -137,11 +137,11 @@ def generate_plot2(start, finish, vel, acc, jerk):
 st.title("Interactive Motion Profile Generator")
 
 # Sidebar Inputs
-param1 = st.sidebar.number_input("Start Position  \n[(User Units)]", value=0,icon=':material/line_start_circle:')
-param2 = st.sidebar.number_input("End Position  \n[(User Units)]", value=50,icon=':material/line_end_circle:')
-param3 = st.sidebar.number_input("Speed  \n[(User Units)/sec]", value=1200,icon=':material/speed:')
-param4 = st.sidebar.number_input("Acc & Dec  \n[(User Units)/sec^$2$]", value=24000,icon=':material/motion_blur:')
-param6 = st.sidebar.number_input("Jerk  \n[(User Units)/sec^$3$]", value=600000,icon=':material/fast_forward:')
+param1 = st.sidebar.number_input("Start Position  \n[(User Units)]", value=0,icon=':material/line_start_circle:', format="%.3f")
+param2 = st.sidebar.number_input("End Position  \n[(User Units)]", value=50,icon=':material/line_end_circle:', format="%.3f")
+param3 = st.sidebar.number_input("Speed  \n[(User Units)/sec]", value=1200,icon=':material/speed:', format="%.3f")
+param4 = st.sidebar.number_input("Acc & Dec  \n[(User Units)/sec^$2$]", value=24000,icon=':material/motion_blur:', format="%.3f")
+param6 = st.sidebar.number_input("Jerk  \n[(User Units)/sec^$3$]", value=600000,icon=':material/fast_forward:', format="%.3f")
 
 # Generate and Display Plot
 if st.button("Generate Plot"):
